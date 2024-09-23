9b97b7d35aa1221fc59d1dbd8838ef516649ce8f
md shorif <mdshorifuddin463@gmail.com>
2024-09-23 13:18:11 +0600

    blog section & common function done

---
+++ js/utilize.js
@@ -0,0 +1,22 @@
+
+
+    // ! get input value 
+
+
+    function getInputValue(id) {
+        let inputValue = document.getElementById(id).value;
+        return inputValue;
+    }
+
+
+
+
+
+
+    // ! get id
+
+
+     function  getId(id) {
+        let id =  document.getElementById(id);
+        return id;
+    }
